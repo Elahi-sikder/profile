0a27c99675d440016ca7d7d6fed570887804af46
ELAHI Sikder <77626169+Elahi-sikder@users.noreply.github.com>
2021-04-30 23:49:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -27,45 +27,22 @@ $(document).ready(function(){
           autoStart: true,
           startDelay: 100
         });
-/*=====================isotope js ==============================
+/*=====================filterizer js ==============================
 ==================================================================*/
-
-        let $grid = $('.grid').isotope({
-        itemSelector: '.element-item',
-        layoutMode: 'fitRows',
-        });
-
-        // filter functions
-        let filterFns = {
-        // show if number is greater than 50
-        numberGreaterThan50: function() {
-          let number = $(this).find('.number').text();
-          return parseInt( number, 10 ) > 50;
-        },
-        // show if name ends with -ium
-        ium: function() {
-          let name = $(this).find('.name').text();
-          return name.match( /ium$/ );
-        }
-        };
-
-        // bind filter button click
-        $('#filters').on( 'click', 'button', function() {
-        let filterValue = $( this ).attr('data-filter');
-        // use filterFn if matches value
-        filterValue = filterFns[ filterValue ] || filterValue;
-        $grid.isotope({ filter: filterValue });
-        });
-
-
-        // change is-checked class on buttons
-        $('.button-group').each( function( i, buttonGroup ) {
-        let $buttonGroup = $( buttonGroup );
-        $buttonGroup.on( 'click', 'button', function() {
-          $buttonGroup.find('.is-checked').removeClass('is-checked');
-          $( this ).addClass('is-checked');
-        });
-        });
+$('.list').click(function(){
+  const value =$(this).attr('data-filter');
+  if(value == "All"){
+    $(".filtr-item").show(200)
+  }else{
+    $(".filtr-item").not('.'+ value).hide();
+    $(".filtr-item").filter('.'+ value).show();
+  }
+  
+})
+$('.list').click(function(){
+  $(this).addClass("active")
+})
+      
 
 /*=====================Mobile menu js ==============================
 ===================================================================*/
